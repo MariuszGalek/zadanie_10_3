5aea9b37f176f9b4a64fbcc2faba8b41d60cf584
Mariusz Galek <mgalek1@gmail.com>
2018-01-14 22:54:39 +0100

    Correct js text formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,55 @@
 $(function(){
-});
 
-var carouselList = $("#carousel ul");
-function defaultInterval() {
-	startInterval = setInterval(nextSlide, 3000);
-}
-defaultInterval();
+	var carouselList = $("#carousel ul");
+	
+	function defaultInterval() {
+		startInterval = setInterval(nextSlide, 3000);
+	}
+	defaultInterval();
 
-function nextSlide() {
-	carouselList.animate({'marginLeft':-600}, 500, moveFirstSlide);
+	function nextSlide() {
+		carouselList.animate({
+			'marginLeft':-600
+		}, 500, moveFirstSlide);
 	}
 
-function moveFirstSlide() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem);
-	var activeItem = carouselList.find('li:first');
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		var activeItem = carouselList.find('li:first');
+		$('.fa-circle:eq('+ firstItem.attr('id') + ')').css("color", "white");
+		$('.fa-circle:eq('+ activeItem.attr('id') + ')').css("color", "red");
+		carouselList.css({
+			marginLeft: 0
+		});
+	}
 
-	$('.fa-circle:eq('+ firstItem.attr('id') + ')').css("color", "white");
-	$('.fa-circle:eq('+ activeItem.attr('id') + ')').css("color", "red");
-
-	carouselList.css({
-		marginLeft: 0
+	$("#right").click(function() {	
+		clearInterval(startInterval);
+		nextSlide();
+		defaultInterval();
 	});
-}
-$("#right").click(function() {	
-	clearInterval(startInterval);
-	nextSlide();
-	defaultInterval();
-});
-$("#left").click(previousSlide);
+
+	$("#left").click(previousSlide);
+	
 	function previousSlide() {
 		clearInterval(startInterval);
 		moveFirstSlideBack();
-		carouselList.animate({'marginLeft': 0}, 500, defaultInterval);
-	};
+		carouselList.animate({
+			'marginLeft': 0
+		}, 500, defaultInterval);
+	}
 
-function moveFirstSlideBack() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	firstItem.before(lastItem);
-	var activeItem = carouselList.find('li:first');
-
-	$('.fa-circle:eq('+ firstItem.attr('id') + ')').css("color", "white");
-	$('.fa-circle:eq('+ activeItem.attr('id') + ')').css("color", "red");
-
-	carouselList.css({
+	function moveFirstSlideBack() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		var activeItem = carouselList.find('li:first');
+		$('.fa-circle:eq('+ firstItem.attr('id') + ')').css("color", "white");
+		$('.fa-circle:eq('+ activeItem.attr('id') + ')').css("color", "red");
+		carouselList.css({
 			marginLeft: -600
 		});
-	};
+	}
+});
